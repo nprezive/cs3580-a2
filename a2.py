67f8cba939d4af2284e8295d12cf69be6d3d13da
nprezive <npreszler@gmail.com>
2018-01-30 21:05:43 -0700

    Finished Part 1.

--- a2.py
+++ a2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from collections import defaultdict
+import operator
 import pprint
 
 def getRaceDict():
@@ -13,8 +14,9 @@ def mk_float(s):
 
 #Part 1
 def part1():
+    #Make a dictionary of states and their race populations
     stateDict = defaultdict(lambda: getRaceDict())
-    with open('/home/nprezive/School/CS3580/a2/acs2015_census_tract_data.csv') as f:
+    with open('acs2015_census_tract_data.csv') as f:
         next(f)
         for line in f:
             lineList = line.split(",")
@@ -26,39 +28,78 @@ def part1():
             stateDict[lineList[1]]["Pacific"] += (mk_int(lineList[3]) * mk_float(lineList[11]) / 100)
     stateDict.pop('District of Columbia')
     stateDict.pop('Puerto Rico')
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(stateDict)
 
-    print("\n\n\n")
+    #Map dictionary to a dictionary of states and their race proportions
+    stateDict2 = dict()
+    for k, v in stateDict.items():
+        stateTotalPop = sum(v.values())
+        stateDict2[k] = {'Hispanic': ( v['Hispanic'] / stateTotalPop ),
+                         'White': ( v['White'] / stateTotalPop ), 
+                         'Black': ( v['Black'] / stateTotalPop ),
+                         'Native': ( v['Native'] / stateTotalPop ),
+                         'Asian': ( v['Asian'] / stateTotalPop ),
+                         'Pacific': ( v['Pacific'] / stateTotalPop )}
 
-    stateDict2 = defaultdict(lambda: getRaceDict())
-    with open('/home/nprezive/School/CS3580/a2/acs2015_county_data.csv') as f:
-        next(f)
-        for line in f:
-            lineList = line.split(",")
-            stateDict2[lineList[1]]["Hispanic"] += (mk_int(lineList[3]) * mk_float(lineList[6]) / 100)
-            stateDict2[lineList[1]]["White"] += (mk_int(lineList[3]) * mk_float(lineList[7]) / 100)   
-            stateDict2[lineList[1]]["Black"] += (mk_int(lineList[3]) * mk_float(lineList[8]) / 100)          
-            stateDict2[lineList[1]]["Native"] += (mk_int(lineList[3]) * mk_float(lineList[9]) / 100)           
-            stateDict2[lineList[1]]["Asian"] += (mk_int(lineList[3]) * mk_float(lineList[10]) / 100)           
-            stateDict2[lineList[1]]["Pacific"] += (mk_int(lineList[3]) * mk_float(lineList[11]) / 100)
-    stateDict2.pop('District of Columbia')
-    stateDict2.pop('Puerto Rico')
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(stateDict2)    
+    #Asian
+    stateAsianProp = dict()
+    for k, v in stateDict2.items():
+        stateAsianProp[k] = v['Asian']
+    sortedAsian = sorted(stateAsianProp.items(), 
+                            key=operator.itemgetter(1), 
+                            reverse=True)
+    print("Part 1:")
+    print("Asian: " + str(sortedAsian[0][0]))
 
-def test1():
-    stateDict = defaultdict(lambda: getRaceDict())
-    with open('/home/nprezive/School/CS3580/a2/acs2015_census_tract_data.csv') as f:
-        next(f)
-        for line in f:
-            lineList = line.split(",")
-            print("{0}, {1}, {2}".format(lineList[1], lineList[3], lineList[6]))
-            stateDict[lineList[1]]['Hispanic'] += (int(lineList[3]) * float(lineList[6]) / 100)
+    #Black
+    stateBlackProp = dict()
+    for k, v in stateDict2.items():
+        stateBlackProp[k] = v['Black']
+    sortedBlack = sorted(stateBlackProp.items(), 
+                            key=operator.itemgetter(1), 
+                            reverse=True)
+    print("Black: " + str(sortedBlack[0][0]))
+
+    #Hispanic
+    stateHispanicProp = dict()
+    for k, v in stateDict2.items():
+        stateHispanicProp[k] = v['Hispanic']
+    sortedHispanic = sorted(stateHispanicProp.items(), 
+                            key=operator.itemgetter(1), 
+                            reverse=True)
+    print("Hispanic: " + str(sortedHispanic[0][0]))
+
+    #Native
+    stateNativeProp = dict()
+    for k, v in stateDict2.items():
+        stateNativeProp[k] = v['Native']
+    sortedNative = sorted(stateNativeProp.items(), 
+                            key=operator.itemgetter(1), 
+                            reverse=True)
+    print("Native: " + str(sortedNative[0][0]))
+
+    #Pacific
+    statePacificProp = dict()
+    for k, v in stateDict2.items():
+        statePacificProp[k] = v['Pacific']
+    sortedPacific = sorted(statePacificProp.items(), 
+                            key=operator.itemgetter(1), 
+                            reverse=True)
+    print("Pacific: " + str(sortedPacific[0][0]))
+
+    #White
+    stateWhiteProp = dict()
+    for k, v in stateDict2.items():
+        stateWhiteProp[k] = v['White']
+    sortedWhite = sorted(stateWhiteProp.items(), 
+                            key=operator.itemgetter(1), 
+                            reverse=True)
+    print("White: " + str(sortedWhite[0][0]))
+
+
 
 
 
 
 #Run assignment
-print("Name: Noah Preszler")
+print("Name: Noah Preszler\n")
 part1()
